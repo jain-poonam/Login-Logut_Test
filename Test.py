dd9b46d8dbcd6e01c077a0d61864379ca7c37c05
Poonam Jain <pjain3326@gmail.com>
2019-01-25 17:14:26 -0800

    Updated

--- Test.py
+++ Test.py
@@ -1,13 +1,13 @@
 """
 Implement Login/Logout Test for Gmail Application in Android. 
-Tool and tests, to check if login and logout is ok.
+Tool and tests, to automate login and logout
 """
  
 import os
 import unittest
 from appium import webdriver
-from time import sleep
- 
+
+
 class GmailAndroidTests(unittest.TestCase):
     "Class to run tests against the Gmail app"
     def setUp(self):
@@ -16,8 +16,6 @@ class GmailAndroidTests(unittest.TestCase):
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '9'
         desired_caps['deviceName'] = 'Pixel'
-        # Returns abs path relative to this file and not cwd
-#         desired_caps['app'] = os.path.abspath(os.path.join(os.path.dirname(__file__),'apps/gmail.apk'))
         desired_caps['app'] = '/Users/pyjain/Downloads/Gmail-7.3.26.152772569.release.apk'
         desired_caps['appPackage'] = 'com.google.android.gm'
         desired_caps['appActivity'] = 'com.google.android.gm.ui.MailActivityGmail'
@@ -28,22 +26,22 @@ class GmailAndroidTests(unittest.TestCase):
         self.driver.quit()
    
     def test_mode(self):
-        file1 = open("/Users/pyjain/Desktop/interview.txt").read().splitlines()
-        email = file1[0]
-        pwd = file1[1]
-        self.driver.implicitly_wait(90)
-        element = self.driver.find_element_by_id("com.google.android.gm:id/welcome_tour_got_it")
-        element.click()
         
+        file1 = open("/Users/pyjain/Desktop/email_credential.txt").read().splitlines()
+        email = file1[0] #Read email id from the given text file 
+        pwd = file1[1] #Read password from the given text file
+        self.driver.implicitly_wait(90)  #implicitly wait for an element to be found,or a command to complete
+        
+        "Find element by id and resource id has been generated with the help of  the uiautomatorviewer by taking screenshots"
+        element = self.driver.find_element_by_id("com.google.android.gm:id/welcome_tour_got_it")
+        element.click() #Clicks the element provided by the id
         
         element = self.driver.find_element_by_id("com.google.android.gm:id/setup_addresses_add_another")
         element.click()
         
-        
         element = self.driver.find_element_by_id("com.google.android.gm:id/account_setup_item")
         element.click()
         
-
         element = self.driver.find_element_by_id("identifierId")
         element.click()
         
@@ -66,8 +64,7 @@ class GmailAndroidTests(unittest.TestCase):
         
         element = self.driver.find_element_by_id("com.google.android.gms:id/next_button")
         element.click()
-        
-         
+             
         element = self.driver.find_element_by_id("com.google.android.gm:id/account_address")
         element.click()
  
